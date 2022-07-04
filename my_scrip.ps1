edbde9e5e646041a4567dfc8f351916f8bb86231
Jerry Yue <jerry_yue@trendmicro.com>
2022-07-04 16:42:38 +0800

    1111

--- my_scrip.ps1
+++ my_scrip.ps1
@@ -1,3 +1,22 @@
-/bin/sh ls -al
-$date = (get-date).dayofyear
-get-service | out-file "$date.log"
+param ($ComputerName = $(throw "ComputerName parameter is required."))
+
+function CanPing {
+   $error.clear()
+   $tmp = test-connection $computername -erroraction SilentlyContinue
+
+   if (!$?)
+       {write-host "Ping failed: $ComputerName."; return $false}
+   else
+       {write-host "Ping succeeded: $ComputerName"; return $true}
+}
+
+function CanRemote {
+    $s = new-pssession $computername -erroraction SilentlyContinue
+
+    if ($s -is [System.Management.Automation.Runspaces.PSSession])
+        {write-host "Remote test succeeded: $ComputerName."}
+    else
+        {write-host "Remote test failed: $ComputerName."}
+}
+
+if (CanPing $computername) {CanRemote $computername}
